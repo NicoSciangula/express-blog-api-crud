0988f3067e4abf3c1deb64dfef904edbf250bb56
NicoSciangula <nico.sciangula99@gmail.com>
2026-04-16 18:04:54 +0200

    imported middleware into the app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3000;
 const postRouter = require("./routers/postRouter");
 
+// * Middleware custom per errori
+const errorsHandler = require("./middleware/errorsHandler");
+const notFound = require("./middleware/notFound");
+
 app.use(express.json());
 
 app.use("/posts", postRouter);
@@ -10,6 +14,10 @@ app.use("/posts", postRouter);
 app.get("/", (req, res) => {
   res.send("Benvenuto nel mio Blog");
 });
+
+app.use(errorsHandler);
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log("Il mio Blog!");
