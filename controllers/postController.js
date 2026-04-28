2a99bb86582176138148fc49bcbd12ada1f42ea3
NicoSciangula <nico.sciangula99@gmail.com>
2026-04-28 20:54:51 +0200

    added JOIN in the function SHOW to view the tags of the single post

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,13 +16,27 @@ function index(req, res) {
 function show(req, res) {
   const { id } = req.params;
 
-  const sql = "SELECT * FROM posts WHERE id = ?";
+  const postsSql = "SELECT * FROM posts WHERE id = ?";
 
-  connection.query(sql, [id], (err, results) => {
+  const tagsSql = `
+SELECT T.*
+FROM tags T
+JOIN post_tag PT ON T.id = PT.tag_id
+WHERE PT.post_id = ? `;
+
+  connection.query(postsSql, [id], (err, postResults) => {
     if (err) return res.status(500).json({ error: "Database query failed" });
-    if (results.length === 0)
+    if (postResults.length === 0)
       return res.status(404).json({ error: "Post not found" });
-    res.json(results[0]);
+
+    const post = postResults[0];
+
+    connection.query(tagsSql, [id], (err, tagResults) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      post.tags = tagResults;
+      res.json(post);
+    });
   });
 }
 
